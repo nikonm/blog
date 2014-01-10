dda5989bb18ccd4763ba2e8acddbd3b669f8bf28
nikita_mastukov <nikita.mastukov@firstlinesoftware.com>
2014-01-10 16:20:33 +0400

    blog

--- src/NikonM/BlogBundle/Controller/IndexController.php
+++ src/NikonM/BlogBundle/Controller/IndexController.php
@@ -10,22 +10,26 @@ class IndexController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('BlogBundle:Index:index.html.twig',
+        return $this->render('BlogBundle:Index:index.html.twig');
+    }
+
+    public function blogAction()
+    {
+        return $this->render('BlogBundle:Index:blog.html.twig',
             [
                 'nodes' => [
                     [
                         'title' => 'babfab',
                         'commentCount' => 34,
                         'url' => '/fffa/fasf/',
-                        'bodyShort' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+                        'bodyShort' => '<p>Lorem <b>ipsum</b> dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
                     ],  [
                         'title' => 'dshher',
                         'commentCount' => 12,
                         'url' => '/rrw/ffff/',
                         'bodyShort' => '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
                     ]]
-            ]
-        );
+            ]);
     }
 
     public function aboutAction()
